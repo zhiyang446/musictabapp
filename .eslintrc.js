a69de10d0108ecc7ba71974a58f220e31cc02498
Hobart446 <hobart@codaai.tech>
2025-08-14 22:56:18 +0800

    T03: Add code formatting and linting tools (ESLint, Prettier, Ruff, Black)

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // Add custom rules here
+    'no-console': 'warn',
+    'no-unused-vars': 'error',
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'build/',
+    '*.config.js',
+  ],
+};
